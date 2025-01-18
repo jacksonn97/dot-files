5d13bfc69447690c7601a5bd2a48dc9e2f5e3269
jacksonn97 <jackson.n.97@proton.me>
2025-01-18 20:54:07 +0300

    Nvim: Back to vim-script

--- nvim/init.lua
+++
@@ -1,7 +0,0 @@
-require("core/options")
-require("core/keymaps")
-require("core/plugins")
-require("core/colorscheme")
-require("core/autocmds")
-require("core/globals")
-
--- nvim/lua/core/globals.lua
+++
@@ -1,8 +0,0 @@
-P = function (v)
-  print(vim.inspect(v))
-  return v
-end
-
--- RELOAD = function (...)
---   return requi
--- end
